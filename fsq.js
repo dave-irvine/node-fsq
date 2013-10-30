354bc0fe6d073194bc04500c4030fdbe819d761a
Dave Irvine <dave@daveirvine.me>
2013-10-30 17:48:50 +0000

    Added initial code to flesh out ideas in feature branches.

---
+++ fsq.js
@@ -0,0 +1,66 @@
+/* jshint node: true */
+"use strict";
+
+var fs = require("fs"),
+	Q = require("q");
+
+(function () {
+	var fsq = {};
+	var queues = {
+		handles: 0,
+		maxHandles: Number.MAX_VALUE
+	};
+
+	Object.defineProperty(fsq, "handles", {
+		get: function () {
+			return queues.handles;
+		}
+	});
+
+	Object.defineProperty(fsq, "fs", {
+		set: function (fsModule) {
+			fs = fsModule;
+		}
+	});
+
+	fsq.writeFile = function (filename, data, options) {
+		var deferred = Q.defer();
+
+		(function deferredWrite() {
+			var unstack = function () {
+				setTimeout(function () {
+					deferredWrite();
+				});
+			};
+
+			if (queues.handles < queues.maxHandles) {
+				try {
+					queues.handles++;
+					fs.writeFile(filename, data, options, function (err) {
+						queues.handles--;
+						if (err) {
+							console.log(err);
+							if (err.code === "EMFILE") {
+								queues.maxHandles--;
+								unstack();
+							} else {
+								deferred.reject(err);
+							}
+						} else {
+							deferred.resolve();
+						}
+					});
+				} catch(err) {
+					console.log(err);
+					deferred.reject(err);
+				}
+			} else {
+				unstack();
+			}
+		}());
+
+		return deferred.promise;
+	};
+
+	module.exports = fsq;
+}());
---
+++ test/fsq.test.js
@@ -0,0 +1,55 @@
+/* jshint node: true */
+/* global describe */
+/* global it */
+"use strict";
+
+var chai = require("chai"),
+	chaiAsPromised = require("chai-as-promised"),
+	expect = chai.expect,
+	fs = require("fs"),
+	sinon = require("sinon"),
+	sinonChai = require("sinon-chai"),
+	Q = require("q");
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+
+var fsq = require("../fsq");
+
+describe("fsq", function () {
+	var fakefs;
+
+	describe("Property: fs", function () {
+		it("should return undefined", function () {
+			expect(fsq.fs).to.equal(undefined);
+		});
+
+		it("should allow injection of an object to replace internal 'fs' module", function () {
+			var writeFileSpy = sinon.spy();
+
+			fakefs = {
+				writeFile: writeFileSpy
+			};
+
+			fsq.fs = fakefs;
+			fsq.writeFile();
+			expect(writeFileSpy).to.have.been.calledOnce;
+		});
+	});
+
+	describe("Property: handles", function () {
+		it("should return current amount of open file handles", function () {
+			expect(fsq.handles).to.equal(0);
+		});
+	});
+
+	describe("writeFile()", function () {
+		var promise;
+
+		it("should return a Promise", function () {
+			promise = fsq.writeFile();
+
+			expect(promise).to.be.an.instanceOf(Q.makePromise);
+		});
+	});
+});
